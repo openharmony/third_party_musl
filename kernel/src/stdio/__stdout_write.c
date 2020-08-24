c415a6f2f3f43d81f07f99e633ea6d9f2a4b5ed7
z00538436 <z00538436@notesmail.huawei.com>
2020-08-24 23:23:39 +0800

    TicketNo:DTS2020090109976 Description:fix libc(user&kernel) printf func
    Team:OTHERS
    Feature or Bugfix:Bugfix
    Binary Source:NA
    PrivateCode(Yes/No):No
    
    Change-Id: I78d37cd46a65fcbac889e3be76ea6a4360e6e8a9
    Reviewed-on: http://mgit-tm.rnd.huawei.com/10583123
    Tested-by: public jenkins <public_jenkins@notesmail.huawei.com>
    Reviewed-by: maguangyao 00383488 <maguangyao@huawei.com>
    Reviewed-by: zhushengle 00451061 <zhushengle@huawei.com>
    Reviewed-by: jianghan 00316535 <jianghan2@huawei.com>
    Reviewed-by: shenwei 00579521 <denny.shenwei@huawei.com>

--- kernel/src/stdio/__stdout_write.c
+++ kernel/src/stdio/__stdout_write.c
@@ -1,13 +1,17 @@
 #include "stdio_impl.h"
 #include <sys/ioctl.h>
-#include "fs/fs.h"
-#include "fs/file.h"
 
 size_t __stdout_write(FILE *f, const unsigned char *buf, size_t len)
 {
-	struct winsize wsz;
-	f->write = __stdio_write;
-	if (!(f->flags & F_SVB) && ioctl(f->fd, TIOCGWINSZ, &wsz))
-		f->lbf = -1;
-	return __stdio_write(f, buf, len);
+    struct winsize wsz;
+    if (!(f->flags & F_SVB) && ioctl(f->fd, TIOCGWINSZ, &wsz)) {
+        f->lbf = EOF;
+    } else {
+        f->lbf = '\n';
+        f->write = __stdio_write;
+        f->wpos = f->wbase = f->buf;
+        f->wend = f->buf + f->buf_size;
+    }
+    if (len == -1) return 0;
+    return __stdio_write(f, buf, len);
 }
