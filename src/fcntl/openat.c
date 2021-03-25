09df7af0af90728ebdc4cea37d8337bc910a9ae4
maguangyao <maguangyao@huawei.com>
2021-03-25 21:47:11 +0800

    feat: musl adapt for mksh & toybox
    Change-Id: I552790d99c070f045c9300d71c1c69ed5de3ce7b

--- src/fcntl/openat.c
+++ src/fcntl/openat.c
@@ -1,13 +1,11 @@
 #include <fcntl.h>
 #include <stdarg.h>
 #include "syscall.h"
-#include <unsupported_api.h>
 
 int openat(int fd, const char *filename, int flags, ...)
 {
 	mode_t mode = 0;
 
-	unsupported_api(__FUNCTION__);
 	if ((flags & O_CREAT) || (flags & O_TMPFILE) == O_TMPFILE) {
 		va_list ap;
 		va_start(ap, flags);
--- src/unistd/unlinkat.c
+++ src/unistd/unlinkat.c
@@ -1,10 +1,8 @@
 #include <unistd.h>
-#include <unsupported_api.h>
 
 #include "syscall.h"
 
 int unlinkat(int fd, const char *path, int flag)
 {
-	unsupported_api(__FUNCTION__);
 	return syscall(SYS_unlinkat, fd, path, flag);
 }
