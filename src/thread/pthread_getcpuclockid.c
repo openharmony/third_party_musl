f19ac71708a40cd7b2affc8f281538ac485b8716
teamol <guweijie@huawei.com>
2021-04-29 15:42:15 +0800

    fix time mould's musl api:clock_getcpuclockid & pthread_getcpuclockid
    Change-Id: I8224c82a2fb34b0ab6e5b9b100f7de860f95cafe

--- src/thread/pthread_getcpuclockid.c
+++ src/thread/pthread_getcpuclockid.c
@@ -1,9 +1,7 @@
-#include <unsupported_api.h>
 #include "pthread_impl.h"
 
 int pthread_getcpuclockid(pthread_t t, clockid_t *clockid)
 {
-	unsupported_api(__FUNCTION__);
-	*clockid = (-t->tid-1)*8U + 6;
-	return 0;
+        *clockid = (-t->tid-1)*8U + 6;
+        return 0;
 }
--- src/time/clock_getcpuclockid.c
+++ src/time/clock_getcpuclockid.c
@@ -1,17 +1,15 @@
 #include <time.h>
 #include <errno.h>
 #include <unistd.h>
-#include <unsupported_api.h>
 #include "syscall.h"
 
 int clock_getcpuclockid(pid_t pid, clockid_t *clk)
 {
-	struct timespec ts;
-	unsupported_api(__FUNCTION__);
+        struct timespec ts;
 
-	clockid_t id = (-pid-1)*8U + 2;
-	int ret = __syscall(SYS_clock_getres, id, &ts);
-	if (ret) return -ret;
-	*clk = id;
-	return 0;
+        clockid_t id = (-pid-1)*8U + 2;
+        int ret = __syscall(SYS_clock_getres, id, &ts);
+        if (ret) return -ret;
+        *clk = id;
+        return 0;
 }
