586e7676ddb6f5f962d8322dafb5ee0297ae5088
chenjing <chenjing139@huawei.com>
2021-06-04 10:16:14 +0800

    :feat: support link/linkat/symlink/symlinkat/readlink/readlinkat
    close #I3Q0OD
    
    Change-Id: I54f538e265619690889c424488993f1aefaa27ee
    Signed-off-by: chenjing <chenjing139@huawei.com>

--- src/unistd/symlinkat.c
+++ src/unistd/symlinkat.c
@@ -1,10 +1,8 @@
 #include <unistd.h>
-#include <unsupported_api.h>
 
 #include "syscall.h"
 
 int symlinkat(const char *existing, int fd, const char *new)
 {
-	unsupported_api(__FUNCTION__);
 	return syscall(SYS_symlinkat, existing, fd, new);
 }
