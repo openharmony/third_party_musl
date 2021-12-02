97559999f0921b200817d9914ad4478b7d94c237
caifuzhou <504631861@qq.com>
2021-12-02 09:35:28 +0800

    porting里新增接口sched_setscheduler，修改musl_src.gni，新增对应测试用例并修改test_src_functional.gni
    Signed-off-by: caifuzhou <504631861@qq.com>

---
+++ libc-test/src/functional/sched_setscheduler.c
@@ -0,0 +1,28 @@
+#include <sched.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include "test.h"
+
+#define TEST(c) ((!c) ? 1 : (t_error(#c" failed: %s" ,strerror(errno)),0))
+
+int main(int argc,char *argv[])
+{
+    struct sched_param param;
+    int maxpri;
+
+    maxpri = sched_get_priority_max(SCHED_FIFO);
+    if(maxpri == -1) 
+    {
+        perror("get priority failed");
+        return -1;
+    }
+    param.sched_priority = maxpri;
+    TEST(sched_setscheduler(getpid(), SCHED_FIFO, &param));
+    TEST(!(sched_getscheduler(getpid()) == SCHED_FIFO));
+    
+    return 0;
+}
+
