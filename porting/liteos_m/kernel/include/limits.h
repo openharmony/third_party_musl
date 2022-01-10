81d69c02005aae8488d029ecf942e633c2c2caba
zhushengle <zhushengle@huawei.com>
2022-01-10 20:59:31 +0800

    fix: 修复iar下未定义的宏
    Close #I4Q5Q5
    Signed-off-by: zhushengle <zhushengle@huawei.com>
    Change-Id: I21169f1f7ec739cdd74d89b0a8d030f2d2897fb3

--- porting/liteos_m/kernel/include/limits.h
+++ porting/liteos_m/kernel/include/limits.h
@@ -5,10 +5,12 @@
 
 #ifdef __ICCARM__ /* for iar */
 #define PATH_MAX 256
+#define NAME_MAX 255
 #define MQ_PRIO_MAX 1
 #define PTHREAD_STACK_MIN LOSCFG_BASE_CORE_TSK_MIN_STACK_SIZE
 #define IOV_MAX 1024
 #define SSIZE_MAX LONG_MAX
+#define PTHREAD_KEYS_MAX 128
 #include_next <limits.h>
 #else
 
