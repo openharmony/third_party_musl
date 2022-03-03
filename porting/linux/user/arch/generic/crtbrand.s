05e19d9c81dd0bdba494617bb1f9a37f56046201
chenjianwen <chenjianwen15@huawei.com>
2022-03-03 10:44:50 +0800

    Modify crtbrand.s file
    Signed-off-by: chenjianwen <chenjianwen15@huawei.com>

--- porting/linux/user/arch/generic/crtbrand.s
+++ porting/linux/user/arch/generic/crtbrand.s
@@ -5,6 +5,6 @@ abitag:
   .long 2f-1f                 // int32_t namesz
   .long 3f-2f                 // int32_t descsz
 1:.ascii "OHOS\0"          // char name[]
-2:.long 12  // int32_t ohos_api
+2:.long 1  // int32_t ohos_api
 3:
   .size abitag, .-abitag
