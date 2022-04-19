0d7a48ff53b9201333c3251c503d3bbfef538e5c
jiangkuaixue <jiangchenzhou@huawei.com>
2022-04-19 20:24:21 +0800

    fixed 1f893a3 from https://gitee.com/jiangkuaixue/third_party_musl/pulls/282 Revert "!268 Dynamic linker supports namespace mechanism"
    This reverts commit acdab8e8f54dc121c1a51d4ca800ad65c150471f, reversing
    changes made to 412ca9eb1e7553f2d9d38abd1e833856415cb6c0.
    
    Signed-off-by: jiangkuaixue <jiangchenzhou@huawei.com>

--- porting/linux/user/include/dlfcn.h
+++ porting/linux/user/include/dlfcn.h
@@ -25,29 +25,19 @@ void  *dlopen(const char *, int);
 void  *dlsym(void *__restrict, const char *__restrict);
 
 /* namespace apis */
-#define NS_NAME_MAX 255
-typedef struct {
-	char name[NS_NAME_MAX+1];
-} Dl_namespace;
+typedef const char* Dl_namespace;
 
 void dlns_init(Dl_namespace *, const char *);
 
 /* open dso in given namespace which has own lib search paths
  * when namespace is null, it's same to dlopen() 
- * avoid using "default" as namespace, which is the default namespace */
+ * void to use "default" as namespace, which is the default namespace*/
 void *dlopen_ns(Dl_namespace *, const char *, int);
 
 /* create the namespace and set lib search paths of namespace,
  * the paths should be splited by ':'. When namespace already exist, 
  * return error */
 int dlns_create(Dl_namespace *, const char *);
-
-/* make one namespace inherit another, and so it can use shared libs by the inherited one. 
- * param1: namespace, param2: inherited namespace, param3: shared libs.
- * the shared libs should be splited by ':'. when it is null or empty, all libs can be shared.
- * one namespace can inherit or be inherited by multiple ones.
- * When namespaces do not exist, return error */
-int dlns_inherit(Dl_namespace *, Dl_namespace *, const char *);
 
 #if defined(_GNU_SOURCE) || defined(_BSD_SOURCE)
 typedef struct {
