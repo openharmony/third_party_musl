a3f2c9d4b9d0b3aa0b8b5a8654c44ca833fb89c8
maosiping <maosiping@huawei.com>
2022-06-24 16:41:13 +0800

    dns proxt from netsys
    Signed-off-by: maosiping <maosiping@huawei.com>

--- src/network/freeaddrinfo.c
+++ src/network/freeaddrinfo.c
@@ -6,10 +6,12 @@
 
 void freeaddrinfo(struct addrinfo *p)
 {
+	if (!p) {
+		return;
+	}
 	size_t cnt;
-	for (cnt=1; p->ai_next; cnt++, p=p->ai_next);
 	struct aibuf *b = (void *)((char *)p - offsetof(struct aibuf, ai));
-	b -= b->slot;
+	for (cnt=1; p->ai_next; cnt++, p=p->ai_next);
 	LOCK(b->lock);
 	if (!(b->ref -= cnt)) free(b);
 	else UNLOCK(b->lock);
