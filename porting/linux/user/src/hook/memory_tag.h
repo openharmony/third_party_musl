d5648e0390cb0aa6673cab55776a17142026b172
shuxinyia <shuxinyi4@h-partners.com>
2022-06-27 19:59:53 +0800

    增加mmap hook的能力      Signed-off-by:shuxinyiA <zhangcui11@huawei.com>
    Signed-off-by: shuxinyia <shuxinyi4@h-partners.com>

--- porting/linux/user/src/hook/memory_tag.h
+++ porting/linux/user/src/hook/memory_tag.h
@@ -5,11 +5,10 @@
 
 typedef int (*mtypeset)(const void* addr, size_t addrlen, const char* tag, size_t tagLen);
 extern mtypeset __mem_typeset;
-#define MEM_TYPESET(addr, addrlen, tag, tagLen)                   \
-	do {                                                 \
-		if (__mem_typeset != NULL) {                     \
-			__mem_typeset(addr, addrlen, tag, tagLen);            \
-		}                                                \
-	} while (0)
-
+#define MEM_TYPESET(addr, addrlen, tag, tagLen)        \
+    do {                                               \
+        if (__mem_typeset != NULL) {                   \
+            __mem_typeset(addr, addrlen, tag, tagLen); \
+        }                                              \
+    } while (0)
 #endif
