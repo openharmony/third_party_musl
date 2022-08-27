151acaadce492532af82d35cc83f60a756d3e118
jwz <jiangweizheng@huawei.com>
2022-08-27 16:25:53 +0800

    use internal_calloc
    Signed-off-by: jwz <jiangweizheng@huawei.com>

--- porting/linux/user/src/hook/musl_preinit_common.h
+++ porting/linux/user/src/hook/musl_preinit_common.h
@@ -109,6 +109,7 @@ inline volatile const struct MallocDispatchType* get_current_dispatch_table()
 
 #define MUSL_HOOK_PARAM_NAME "libc.hook_mode"
 #define OHOS_PARAM_MAX_SIZE 96
+#define FILE_NAME_MAX_SIZE 40
 
 #ifdef __cplusplus
 }
