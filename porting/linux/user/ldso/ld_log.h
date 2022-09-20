67f20f2801c5c35aa4400ea06a4c9393023c3418
ganlan <tony.gan@huawei.com>
2022-09-21 01:22:37 +0800

    通过系统属性进行linker log配置
    Signed-off-by: ganlan <tony.gan@huawei.com>

--- porting/linux/user/ldso/ld_log.h
+++ porting/linux/user/ldso/ld_log.h
@@ -18,37 +18,56 @@
 
 #include <musl_log.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #define LD_LOG_ERROR 1
 #define LD_LOG_WARNING 2
 #define LD_LOG_INFO 4
 #define LD_LOG_DEBUG 8
 
-#define LD_LOG_LEVEL (LD_LOG_ERROR | LD_LOG_WARNING)
+#define LD_LOG_LEVEL (LD_LOG_ERROR | LD_LOG_WARNING | LD_LOG_INFO)
 
 #define LD_LOG_TAG "MUSL-LDSO"
 
-#if (defined(ENABLE_MUSL_LOG) && (LD_LOG_LEVEL & LD_LOG_ERROR))
-#define LD_LOGE(...) ((void)HiLogAdapterPrint(MUSL_LOG_TYPE, LOG_ERROR, MUSL_LOG_DOMAIN, LD_LOG_TAG, __VA_ARGS__))
+hidden bool get_ld_log_enable();
+hidden void ld_log_reset();
+
+#if ((LD_LOG_LEVEL & LD_LOG_ERROR) && (defined(OHOS_ENABLE_PARAMETER) || defined(ENABLE_MUSL_LOG)))
+#define LD_LOGE(...) if (get_ld_log_enable()) {   \
+    (void)HiLogAdapterPrint(MUSL_LOG_TYPE, LOG_ERROR, MUSL_LOG_DOMAIN, LD_LOG_TAG, __VA_ARGS__);    \
+}
 #else
 #define LD_LOGE(...)
 #endif
 
-#if (defined(ENABLE_MUSL_LOG) && (LD_LOG_LEVEL & LD_LOG_WARNING))
-#define LD_LOGW(...) ((void)HiLogAdapterPrint(MUSL_LOG_TYPE, LOG_WARN, MUSL_LOG_DOMAIN, LD_LOG_TAG, __VA_ARGS__))
+#if ((LD_LOG_LEVEL & LD_LOG_WARNING) && (defined(OHOS_ENABLE_PARAMETER) || defined(ENABLE_MUSL_LOG)))
+#define LD_LOGW(...) if (get_ld_log_enable()) {   \
+    (void)HiLogAdapterPrint(MUSL_LOG_TYPE, LOG_WARN, MUSL_LOG_DOMAIN, LD_LOG_TAG, __VA_ARGS__); \
+}
 #else
 #define LD_LOGW(...)
 #endif
 
-#if (defined(ENABLE_MUSL_LOG) && (LD_LOG_LEVEL & LD_LOG_INFO))
-#define LD_LOGI(...) ((void)HiLogAdapterPrint(MUSL_LOG_TYPE, LOG_INFO, MUSL_LOG_DOMAIN, LD_LOG_TAG, __VA_ARGS__))
+#if ((LD_LOG_LEVEL & LD_LOG_INFO) && (defined(OHOS_ENABLE_PARAMETER) || defined(ENABLE_MUSL_LOG)))
+#define LD_LOGI(...) if (get_ld_log_enable()) {   \
+    (void)HiLogAdapterPrint(MUSL_LOG_TYPE, LOG_INFO, MUSL_LOG_DOMAIN, LD_LOG_TAG, __VA_ARGS__);    \
+}
 #else
 #define LD_LOGI(...)
 #endif
 
-#if (defined(ENABLE_MUSL_LOG) && (LD_LOG_LEVEL & LD_LOG_DEBUG))
-#define LD_LOGD(...) ((void)HiLogAdapterPrint(MUSL_LOG_TYPE, LOG_DEBUG, MUSL_LOG_DOMAIN, LD_LOG_TAG, __VA_ARGS__))
+#if (LD_LOG_LEVEL & LD_LOG_DEBUG) && (defined(OHOS_ENABLE_PARAMETER) || defined(ENABLE_MUSL_LOG))
+#define LD_LOGD(...) if (get_ld_log_enable()) {   \
+    (void)HiLogAdapterPrint(MUSL_LOG_TYPE, LOG_DEBUG, MUSL_LOG_DOMAIN, LD_LOG_TAG, __VA_ARGS__);    \
+}
 #else
 #define LD_LOGD(...)
+#endif
+
+#ifdef __cplusplus
+}
 #endif
 
 #endif  // LD_LOG_H
