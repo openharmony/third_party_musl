20836ff34a6e4161f73f11720567b72220834e18
Maxim Polyakov <polyakov.maksim@huawei.com>
2022-09-29 20:31:56 +0300

    Fix indentation warnings
    Signed-off-by: Maxim Polyakov <polyakov.maksim@huawei.com>
    Change-Id: I5d319e3c7c0a713f3a6d39ba97c28b3ee24807d2
    Signed-off-by: Maxim Polyakov <polyakov.maksim@huawei.com>

--- porting/linux/user/src/thread/pthread_getspecific.c
+++ porting/linux/user/src/thread/pthread_getspecific.c
@@ -3,8 +3,8 @@
 
 static void *__pthread_getspecific(pthread_key_t k)
 {
-	struct pthread *self = __pthread_self();
-	return self->tsd[k];
+    struct pthread *self = __pthread_self();
+    return self->tsd[k];
 }
 
 weak_alias(__pthread_getspecific, pthread_getspecific);
