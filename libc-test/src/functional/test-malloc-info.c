f55c9c8a756c27bcd28819875b4b2e23af03ec0c
Maxim Polyakov <polyakov.maksim@huawei.com>
2022-10-04 17:20:11 +0300

    Fixed a various warnings
    Signed-off-by: Maxim Polyakov <polyakov.maksim@huawei.com>

--- libc-test/src/functional/test-malloc-info.c
+++ libc-test/src/functional/test-malloc-info.c
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <stdlib.h>
 
 #include "test-malloc-info-stats-print.h"
@@ -27,8 +42,7 @@ static const xmlChar *get_attribute(const char *attr_name, xmlNodePtr node)
 	return NULL;
 }
 
-static xmlNodePtr
-find_child_node_with_attr(const char *name, const char *attr_name, const char *attr_value, xmlNodePtr parent)
+static xmlNodePtr find_child_node_with_attr(const char *name, const char *attr_name, const char *attr_value, xmlNodePtr parent)
 {
 	if (parent == NULL)
 	{
