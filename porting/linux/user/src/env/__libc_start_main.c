7db2b604bb8e9b3c6152df8b4dd4f01470cbf238
openharmony_ci <120357966@qq.com>
2022-10-13 12:30:37 +0000

    回退 'Pull Request !567 : Fix linker configuration for the xts-related build'

--- porting/linux/user/src/env/__libc_start_main.c
+++ porting/linux/user/src/env/__libc_start_main.c
@@ -6,11 +6,7 @@
 #include "syscall.h"
 #include "atomic.h"
 #include "libc.h"
-#include "pthread.h"
-#include "sys/mman.h"
-#include "malloc_impl.h"
 #include "pthread_impl.h"
-
 
 static void dummy(void) {}
 weak_alias(dummy, _init);
@@ -98,14 +94,6 @@ static int libc_start_main_stage2(int (*main)(int,char **,char **), int argc, ch
 	pthread_reserve_signal_stack();
 #endif
 	errno = 0;
-
-#ifdef MUSL_ITERATE_AND_STATS_API
-	__init_occupied_bin_key_once();
-	occupied_bin_t *occupied_bin = internal_calloc(sizeof(occupied_bin_t), 1);
-	if (occupied_bin == NULL) return ENOMEM;
-	pthread_setspecific(__get_occupied_bin_key(), occupied_bin);
-#endif
-	libc.initialized = 1;
 
 	/* Pass control to the application */
 	exit(main(argc, argv, envp));
