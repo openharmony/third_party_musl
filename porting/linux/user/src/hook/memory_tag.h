90f8a40bc9f3f996016ea5efcda88ba418ad3c35
lutao <lutao31@huawei.com>
2023-02-08 15:32:08 +0800

    add mem trace
    Signed-off-by: lutao <lutao31@huawei.com>

--- porting/linux/user/src/hook/memory_tag.h
+++ porting/linux/user/src/hook/memory_tag.h
@@ -1,14 +1,20 @@
 #ifndef _MEMORY_TAG_H
 #define _MEMORY_TAG_H
 
+#include <ctype.h>
 #include <unistd.h>
+#include "common_def.h"
+#include "musl_preinit_common.h"
 
-typedef int (*mtypeset)(const void* addr, size_t addrlen, const char* tag, size_t tagLen);
-extern mtypeset __mem_typeset;
-#define MEM_TYPESET(addr, addrlen, tag, tagLen)        \
-    do {                                               \
-        if (__mem_typeset != NULL) {                   \
-            __mem_typeset(addr, addrlen, tag, tagLen); \
-        }                                              \
+typedef int (*mmemtrace)(void* addr, size_t size, const char* tag, bool is_using);
+extern volatile atomic_llong __mem_trace;
+#define MEM_TRACE(addr, size, tag, is_using)                                                                    \
+    do {                                                                                                        \
+        volatile mmemtrace func = (volatile mmemtrace)atomic_load_explicit(&__mem_trace, memory_order_acquire); \
+        if (__predict_false(!__get_global_hook_flag())) {                                                       \
+            if (func != NULL) {                                                                                 \
+                func(addr, size, tag, is_using);                                                                \
+            }                                                                                                   \
+        }                                                                                                       \
     } while (0)
 #endif
