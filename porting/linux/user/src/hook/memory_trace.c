900f1dce5aef8706764d1c627671f181ea32fe32
lutao <lutao31@huawei.com>
2023-02-17 17:22:38 +0800

    add memtrace interface
    Signed-off-by: lutao <lutao31@huawei.com>

---
+++ porting/linux/user/src/hook/memory_trace.c
@@ -0,0 +1,16 @@
+#include "memory_trace.h"
+#ifdef HOOK_ENABLE
+#include "common_def.h"
+#include "musl_preinit_common.h"
+#endif
+
+void memtrace(void* addr, size_t size, const char* tag, bool is_using)
+{
+#ifdef HOOK_ENABLE
+    volatile const struct MallocDispatchType* dispatch_table = get_current_dispatch_table();
+	if (__predict_false(dispatch_table != NULL)) {
+		dispatch_table->memtrace(addr, size, tag, is_using);
+	}
+#endif
+    return;
+}
