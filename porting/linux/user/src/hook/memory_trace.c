1d7de4635682a90be7cc82a859b3f24c951d463b
lutao <lutao31@huawei.com>
2023-06-29 11:27:47 +0800

    Improve memtrace performance issue:#I7GYZ1 Test:libc-test
    Signed-off-by: lutao <lutao31@huawei.com>

--- porting/linux/user/src/hook/memory_trace.c
+++ porting/linux/user/src/hook/memory_trace.c
@@ -7,9 +7,19 @@
 void memtrace(void* addr, size_t size, const char* tag, bool is_using)
 {
 #ifdef HOOK_ENABLE
-	volatile const struct MallocDispatchType* dispatch_table = get_current_dispatch_table();
+	volatile const struct MallocDispatchType* dispatch_table = (struct MallocDispatchType *)atomic_load_explicit(
+		&__musl_libc_globals.current_dispatch_table, memory_order_acquire);
 	if (__predict_false(dispatch_table != NULL)) {
-		dispatch_table->memtrace(addr, size, tag, is_using);
+		if (__get_memleak_hook_flag()) {
+			return;
+		}
+		if (!__get_global_hook_flag()) {
+			return;
+		}
+		else if (!__get_hook_flag()) {
+			return;
+		}
+		return dispatch_table->memtrace(addr, size, tag, is_using);
 	}
 #endif
     return;
