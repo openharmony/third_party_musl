9a6e783ace6e3e4d7689bd621c756e31a6f3224e
zzulilyw <liuyawei10@huawei.com>
2023-08-28 03:14:54 +0000

    Supply services feature
    Issue: https://gitee.com/openharmony/third_party_musl/issues/I7V021
    
    test: libctest
    Signed-off-by: zzulilyw <liuyawei10@huawei.com>

--- libc-test/src/functionalext/supplement/network/getservbyname.c
+++ libc-test/src/functionalext/supplement/network/getservbyname.c
@@ -15,6 +15,11 @@
 
 #include <netdb.h>
 #include "functionalext.h"
+#define FTP_PORT 21
+#define HTTP_PORT 80
+#define TCPMUX_PORT 1
+#define FIDO_PORT 60179
+#define NOT_EXIST_PORT 62000
 
 /**
  * @tc.name      : getservbyname_0100
@@ -45,9 +50,74 @@ void getservbyname_0200(void)
     EXPECT_PTREQ("getservbyname_0200", se, NULL);
 }
 
+/**
+ * @tc.name      : getservbyname_0300
+ * @tc.desc      : Get the service information of the specified port
+ * @tc.level     : Level 0
+*/
+void getservbyname_0300(void)
+{
+    struct servent *se = getservbyname("ftp", "tcp");
+    EXPECT_PTRNE("getservbyname_0300", se, NULL);
+    EXPECT_EQ("getservbyname_0300", ntohs(se->s_port), FTP_PORT);
+}
+
+/**
+ * @tc.name      : getservbyname_0400
+ * @tc.desc      : Get the service information of the specified port
+ * @tc.level     : Level 0
+*/
+void getservbyname_0400(void)
+{
+    struct servent *se = getservbyname("http", "tcp");
+    EXPECT_PTRNE("getservbyname_0400", se, NULL);
+    EXPECT_EQ("getservbyname_0400", ntohs(se->s_port), HTTP_PORT);
+}
+
+/**
+ * @tc.name      : getservbyname_0500
+ * @tc.desc      : Get the service information of the specified port
+ * @tc.level     : Level 0
+*/
+void getservbyname_0500(void)
+{
+    struct servent *se = getservbyname("tcpmux", "tcp");
+    EXPECT_PTRNE("getservbyname_0500", se, NULL);
+    EXPECT_EQ("getservbyname_0500", ntohs(se->s_port), TCPMUX_PORT);
+}
+
+/**
+ * @tc.name      : getservbyname_0600
+ * @tc.desc      : Get the service information of the specified port
+ * @tc.level     : Level 0
+*/
+void getservbyname_0600(void)
+{
+    struct servent *se = getservbyname("fido", "tcp");
+    EXPECT_PTRNE("getservbyname_0600", se, NULL);
+    EXPECT_EQ("getservbyname_0600", ntohs(se->s_port), FIDO_PORT);
+}
+
+/**
+ * @tc.name      : getservbyname_0700
+ * @tc.desc      : Get the service information of the specified port
+ * @tc.level     : Level 0
+*/
+void getservbyname_0700(void)
+{
+    struct servent *se = getservbyname("not_exist_service_name", "tcp");
+    EXPECT_PTREQ("getservbyname_0700", se, NULL);
+}
+
+
 int main(void)
 {
     getservbyname_0100();
     getservbyname_0200();
+    getservbyname_0300();
+    getservbyname_0400();
+    getservbyname_0500();
+    getservbyname_0600();
+    getservbyname_0700();
     return t_status;
 }
