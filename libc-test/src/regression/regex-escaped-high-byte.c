ee5f4d49a3140a39fa9217071750a800fd816958
li-tao116 <litao@kaihong.com>
2023-11-14 18:16:40 -0800

    Optimize regcomp: fix libctest regex-escaped-high-byte
    Use the implementation of openbsd.
    
    Issue: #I8G8HX
    
    Signed-off-by: litao <litao@kaihong.com>
    Change-Id: I152252c88fc69daff70d87eeb89219b534bd3b98

--- libc-test/src/regression/regex-escaped-high-byte.c
+++ libc-test/src/regression/regex-escaped-high-byte.c
@@ -12,11 +12,10 @@ int main(void)
 
 	setlocale(LC_CTYPE, "C.UTF-8");
 
-	// illegal sequence (not U+00FC)
 	n = regcomp(&r, "\\\xfc", 0);
-	if (n != REG_BADPAT) {
+	if (n != 0) {
 		regerror(n, &r, buf, sizeof buf);
-		t_error("regcomp(\\\\\\xfc) returned %d (%s) wanted REG_BADPAT\n", n, buf);
+		t_error("regcomp(\\\\\\xfc) returned %d (%s) wanted 0\n", n, buf);
 	}
 
 	return t_status;
