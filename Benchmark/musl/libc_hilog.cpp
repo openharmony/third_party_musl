55863e845d50d58394619dedd62c6066da022730
zzulilyw <liuyawei10@huawei.com>
2024-03-05 21:33:24 +0800

    hilog_adapter performance and function optimization Issue: https://gitee.com/openharmony/third_party_musl/issues/I8XPUZ Test:Build & Boot devices & xts
    Signed-off-by: zzulilyw <liuyawei10@huawei.com>

--- Benchmark/musl/libc_hilog.cpp
+++ Benchmark/musl/libc_hilog.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) Huawei Technologies Co., Ltd. 2024. All rights reserved.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,50 +12,55 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-#define _GNU_SOURCE
-
+#include <stdarg.h>
 #include <hilog_adapter.h>
-
-#include <errno.h>
-#include <stdint.h>
-#include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <sys/time.h>
-#include <sys/uio.h>
-#include <sys/un.h>
-#include <time.h>
-#include <unistd.h>
 #include <pthread.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/un.h>
+#include <sys/uio.h>
+#include "util.h"
+#define SOCKET_FILE_DIR "/dev/unix/socket/"
+#define INPUT_SOCKET_NAME "hilogInput"
 
-#include "hilog_common.h"
-#ifdef OHOS_ENABLE_PARAMETER
-#include "sys_param.h"
-#endif
-#include "vsnprintf_s_p.h"
-
+#define MUSL_LOG_TYPE LOG_CORE
+#define MUSL_LOG_DOMAIN 0xD003F00
+#define MUSL_LOG_TAG "MUSL"
+#define CLOSE_FD_COUNT 1024
 #define LOG_LEN 3
 #define ERROR_FD 2
-#ifdef OHOS_ENABLE_PARAMETER
-#define SYSPARAM_LENGTH 32
-#endif
+#define MAX_LOG_LEN 1024
+#define MAX_TAG_LEN 32
 
 const int SOCKET_TYPE = SOCK_DGRAM | SOCK_NONBLOCK | SOCK_CLOEXEC;
 const int INVALID_SOCKET = -1;
 const struct sockaddr_un SOCKET_ADDR = {AF_UNIX, SOCKET_FILE_DIR INPUT_SOCKET_NAME};
 
-static bool musl_log_enable = false;
 
-#ifdef OHOS_ENABLE_PARAMETER
-static const char *param_name = "musl.log.enable";
-static const char *g_logLevelParam = "musl.log.level";
-#endif
-static int g_logLevel = LOG_ERROR;
 static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 static volatile int socketFd = INVALID_SOCKET;
+
+/*
+ * header of log message from libhilog to hilogd
+ */
+typedef struct __attribute__((__packed__)) {
+    uint16_t len;
+    uint16_t version : 3;
+    uint16_t type : 4; /* APP,CORE,INIT,SEC etc */
+    uint16_t level : 3;
+    uint16_t tag_len : 6; /* include '\0' */
+    uint32_t tv_sec;
+    uint32_t tv_nsec;
+    uint32_t mono_sec;
+    uint32_t pid;
+    uint32_t tid;
+    uint32_t domain;
+    char tag[]; /* shall be end with '\0' */
+} HilogMsg;
 
 static void cleanup()
 {
@@ -112,7 +117,7 @@ static int SendMessage(HilogMsg *header, const char *tag, uint16_t tagLen, const
     header->len = sizeof(HilogMsg) + tagLen + fmtLen;
     header->tag_len = tagLen;
 
-    struct iovec vec[LOG_LEN] = {0};
+    struct iovec vec[LOG_LEN] = {{0}};
     vec[0].iov_base = header;                   // 0 : index of hos log header
     vec[0].iov_len = sizeof(HilogMsg);          // 0 : index of hos log header
     vec[1].iov_base = (void *)((char *)(tag));  // 1 : index of log tag
@@ -123,13 +128,12 @@ static int SendMessage(HilogMsg *header, const char *tag, uint16_t tagLen, const
     return ret;
 }
 
-HILOG_LOCAL_API
-int HiLogAdapterPrintArgs(
+int HiLogAdapterPrintArgs1(
     const LogType type, const LogLevel level, const unsigned int domain, const char *tag, const char *fmt, va_list ap)
 {
     char buf[MAX_LOG_LEN] = {0};
 
-    vsnprintfp_s(buf, MAX_LOG_LEN, MAX_LOG_LEN - 1, true, fmt, ap);
+    vsnprintf(buf, MAX_LOG_LEN, fmt, ap);
 
     size_t tagLen = strnlen(tag, MAX_TAG_LEN - 1);
     size_t logLen = strnlen(buf, MAX_LOG_LEN - 1);
@@ -145,92 +149,24 @@ int HiLogAdapterPrintArgs(
     return SendMessage(&header, tag, tagLen + 1, buf, logLen + 1);
 }
 
-HILOG_LOCAL_API
-int HiLogAdapterPrint(LogType type, LogLevel level, unsigned int domain, const char *tag, const char *fmt, ...)
+int HiLogAdapterPrint1(LogType type, LogLevel level, unsigned int domain, const char *tag, const char *fmt, ...)
 {
-    if (!HiLogAdapterIsLoggable(domain, tag, level)) {
-        return -1;
-    }
-
     int ret;
     va_list ap;
     va_start(ap, fmt);
-    ret = HiLogAdapterPrintArgs(type, level, domain, tag, fmt, ap);
+    ret = HiLogAdapterPrintArgs1(type, level, domain, tag, fmt, ap);
     va_end(ap);
     return ret;
 }
 
-bool is_musl_log_enable()
+// Used to perform control operations on device files
+// set baud rate
+static void Bm_function_hilog(benchmark::State &state)
 {
-    if (getpid() == 1) {
-        return false;
-    }
-    return musl_log_enable;
-}
-
-bool HiLogAdapterIsLoggable(unsigned int domain, const char *tag, LogLevel level)
-{
-    if (tag == NULL || level < g_logLevel || level <= LOG_LEVEL_MIN || level >= LOG_LEVEL_MAX) {
-        return false;
-    }
-
-    if (!is_musl_log_enable()) {
-        return false;
-    }
-
-    return true;
-}
-
-#ifdef OHOS_ENABLE_PARAMETER
-bool get_bool_sysparam(CachedHandle cachedhandle)
-{
-    const char *param_value = CachedParameterGet(cachedhandle);
-    if (param_value != NULL) {
-        if (strcmp(param_value, "true") == 0) {
-            return true;
-        }
-    }
-    return false;
-}
-
-void resetLogLevel()
-{
-    static CachedHandle muslLogLevelHandle = NULL;
-    if (muslLogLevelHandle == NULL) {
-        muslLogLevelHandle = CachedParameterCreate(g_logLevelParam, "ERROR");
-    }
-    const char *value = CachedParameterGet(muslLogLevelHandle);
-    if (value != NULL) {
-        if (!strcmp(value, "DEBUG")) {
-            g_logLevel = LOG_DEBUG;
-        } else if (!strcmp(value, "INFO")) {
-            g_logLevel = LOG_INFO;
-        } else if (!strcmp(value, "WARN")) {
-            g_logLevel = LOG_WARN;
-        } else if (!strcmp(value, "ERROR")) {
-            g_logLevel = LOG_ERROR;
-        } else if (!strcmp(value, "FATAL")) {
-            g_logLevel = LOG_FATAL;
-        } else {
-            g_logLevel = LOG_ERROR;
-        }
-    } else {
-        g_logLevel = LOG_ERROR;
+    for (auto _ : state) {
+        int ret = HiLogAdapterPrint1(MUSL_LOG_TYPE, LOG_ERROR, MUSL_LOG_DOMAIN, MUSL_LOG_TAG, "musl-hilog-benchmark");
+        benchmark::DoNotOptimize(ret);
     }
 }
 
-#endif
-
-void musl_log_reset()
-{
-#if (defined(OHOS_ENABLE_PARAMETER))
-    static CachedHandle musl_log_Handle = NULL;
-    if (musl_log_Handle == NULL) {
-        musl_log_Handle = CachedParameterCreate(param_name, "false");
-    }
-    musl_log_enable = get_bool_sysparam(musl_log_Handle);
-    resetLogLevel();
-#elif (defined(ENABLE_MUSL_LOG))
-    musl_log_enable = true;
-#endif
-}
+MUSL_BENCHMARK(Bm_function_hilog);
