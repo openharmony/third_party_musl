a1cd91978879c2a861d5a16a0bb64a4fc961f3a1
EurusHomles-zH <zhouhang65@huawei.com>
2024-03-28 19:43:24 +0800

    Description:fdleak track stack Signed-off-by: EurusHomles-zH <zhouhang65@huawei.com>

---
+++ src/hook/linux/musl_fdtrack.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _MUSL_FDTRACK_H
+#define _MUSL_FDTRACK_H
+
+#include <sys/cdefs.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum fdtrack_type {
+	FDTRACK_EVENT_TYPE_CREATE,
+	FDTRACK_EVENT_TYPE_CLOSE,
+};
+
+struct fdtrack_event {
+	int fd;
+	uint32_t type;
+};
+
+typedef void (*fdtrack_hook)(struct fdtrack_event*);
+void set_fdtrack_enabled(bool newValue);
+bool fdtrack_cas_hook(fdtrack_hook* expected, fdtrack_hook value);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
