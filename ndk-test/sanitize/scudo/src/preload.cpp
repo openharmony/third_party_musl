aac9711a5b62c5502d1cd7bee8d5f8f3fe870b1c
liuguangfeng <liuguangfeng2@huawei.com>
2024-03-29 04:05:33 +0000

    Clear alarms
    issue:https://gitee.com/openharmony/third_party_musl/issues/I9CMFA
    
    test:libtest
    
    Signed-off-by: liuguangfeng <liuguangfeng2@huawei.com>

--- ndk-test/sanitize/scudo/src/preload.cpp
+++ ndk-test/sanitize/scudo/src/preload.cpp
@@ -4,9 +4,6 @@
 // RUN: env LD_PRELOAD=%shared_libscudo    not %run %t 2>&1 | FileCheck %s
 // RUN: env LD_PRELOAD=%shared_minlibscudo not %run %t 2>&1 | FileCheck %s
 
-// This way of setting LD_PRELOAD does not work with Android test runner.
-// REQUIRES: !android
-
 #include <assert.h>
 
 int main(int argc, char *argv[]) {
