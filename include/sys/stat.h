c162ef5598b364d203789779f707805005e08289
qijinquan <qijinquan@kaihong.com>
2024-04-02 17:33:44 +0800

    revert modify of remove LFS64 programming interfaces
    Signed-off-by: qijinquan <qijinquan@kaihong.com>

--- include/sys/stat.h
+++ include/sys/stat.h
@@ -153,7 +153,7 @@ struct statx {
 int statx(int, const char *__restrict, int, unsigned, struct statx *__restrict);
 #endif
 
-#if defined(_LARGEFILE64_SOURCE)
+#if defined(_LARGEFILE64_SOURCE) || defined(_GNU_SOURCE)
 #define stat64 stat
 #define fstat64 fstat
 #define lstat64 lstat
