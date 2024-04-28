6d1db6bf25b745ee50e5ab9630b86608a1910996
YinZong <gengyinzong@huawei.com>
2024-04-28 11:49:53 +0000

    raise调用修改为tgkill增加liteos A宏隔离
    Signed-off-by: YinZong <gengyinzong@huawei.com>

--- src/signal/raise.c
+++ src/signal/raise.c
@@ -8,7 +8,11 @@ int raise(int sig)
 {
 	sigset_t set;
 	__block_app_sigs(&set);
+#ifdef __LITEOS__
+        int ret = syscall(SYS_tkill, __pthread_self()->tid, sig);
+#else
 	int ret = syscall(SYS_tgkill, syscall(SYS_getpid), __pthread_self()->tid, sig);
+#endif // #ifdef __LITEOS__
 	__restore_sigs(&set);
 	return ret;
 }
