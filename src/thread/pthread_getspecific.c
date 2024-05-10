5cb76bb068038f22c09f45343ffeff90425c1bce
linyu <yulin24@huawei.com>
2024-05-10 09:19:47 +0800

    FFRT对接TaskPool bugfix
    Signed-off-by: linyu <yulin24@huawei.com>

--- src/thread/pthread_getspecific.c
+++ src/thread/pthread_getspecific.c
@@ -4,14 +4,14 @@
 
 static void *__pthread_getspecific(pthread_key_t k)
 {
-	struct pthread *self = __pthread_self();
-	return self->tsd[k];
+    struct pthread *self = __pthread_self();
+    return self->tsd[k];
 }
 
 void** pthread_gettsd()
 {
-	struct pthread *self = __pthread_self();
-	return self->tsd;
+    struct pthread *self = __pthread_self();
+    return self->tsd;
 }
 
 weak_alias(__pthread_getspecific, pthread_getspecific);
