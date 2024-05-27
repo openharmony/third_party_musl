db3723a69349fe23de5ee755b6af645e6c15f641
openharmony_ci <120357966@qq.com>
2024-05-27 08:23:04 +0000

    回退 'Pull Request !1438 : pthread_mutex_destroy bugfix'

--- src/thread/pthread_mutex_destroy.c
+++ src/thread/pthread_mutex_destroy.c
@@ -9,5 +9,6 @@ int pthread_mutex_destroy(pthread_mutex_t *mutex)
 	if (type > 128) __vm_wait();
 	if (__is_mutex_destroyed(type))
 		__handle_using_destroyed_mutex(mutex, __FUNCTION__);
+	a_store(&mutex->_m_type, PTHREAD_MUTEX_DESTROYED);
 	return 0;
 }
