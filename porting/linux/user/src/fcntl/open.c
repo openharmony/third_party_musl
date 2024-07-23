0fb286c0d5deeab6273e0b4a1910dc8fa2fc2e1d
qiu_yu_jia <qiuyujia1@huawei.com>
2024-07-23 10:48:13 +0800

    modify blue codes to consistend with yellow zone
    Signed-off-by: qiu_yu_jia <qiuyujia1@huawei.com>

--- porting/linux/user/src/fcntl/open.c
+++ porting/linux/user/src/fcntl/open.c
@@ -15,9 +15,6 @@
 
 #include <fcntl.h>
 #include <stdarg.h>
-#ifdef OHOS_FDTRACK_HOOK_ENABLE
-#include "musl_fdtrack_hook.h"
-#endif
 #include "syscall.h"
 
 int open(const char *filename, int flags, ...)
@@ -32,11 +29,7 @@ int open(const char *filename, int flags, ...)
     }
 
     int fd = __sys_open_cp(filename, flags, mode);
-#ifdef OHOS_FDTRACK_HOOK_ENABLE
-    return FDTRACK_START_HOOK(__syscall_ret(fd));
-#else
     return __syscall_ret(fd);
-#endif
 }
 
 weak_alias(open, open64);
