17f24d2ceaf32dda4065281a25382b0af51294e0
zzulilyw <liuyawei10@huawei.com>
2024-07-30 11:11:46 +0800

    Bugfix: fix  incorrect result issue: https://gitee.com/openharmony/third_party_musl/issues/IAGHBG Test: Build & Boot devices & xts
    Signed-off-by: zzulilyw <liuyawei10@huawei.com>

--- src/linux/gettid.c
+++ src/linux/gettid.c
@@ -1,8 +1,16 @@
 #define _GNU_SOURCE
 #include <unistd.h>
+#include "syscall.h"
 #include "pthread_impl.h"
+
+#define ZERO (0)
+#define NEGATIVE_ONE (-1)
 
 pid_t gettid(void)
 {
-	return __pthread_self()->tid;
+	pid_t tid = __pthread_self()->tid;
+	if (tid == ZERO || tid == NEGATIVE_ONE) {
+		return __syscall(SYS_gettid);
+	}
+	return tid;
 }
