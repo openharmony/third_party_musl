ad82b9eb74c4b3257cab0b604a26ac8c8c4c17bf
qiu_yu_jia <qiuyujia1@huawei.com>
2024-08-22 14:03:35 +0800

    modify the codes style
    Signed-off-by: qiu_yu_jia <qiuyujia1@huawei.com>

--- Benchmark/musl/libc_exception.cpp
+++ Benchmark/musl/libc_exception.cpp
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2020-2024. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include "util.h"
 #include <exception>
 #include <cassert>
@@ -8,25 +22,30 @@ static int gThrowExceptionCnt = 0;
 static int gCatchExceptionCnt = 0;
 
 struct TestException : public exception {
-    TestException() {
+    TestException()
+    {
         gThrowExceptionCnt++;
     }
-    virtual const char * what () const _NOEXCEPT {
+    const char* what() const _NOEXCEPT override
+    {
         return "Test Exception!";
     }
 
-    void check_result() {
+    void check_result()
+    {
         gCatchExceptionCnt++;
     }
 };
 
 // increase call stack depth
-void __attribute__((noinline)) ThrowExceptionCallStub() {
+void __attribute__((noinline)) ThrowExceptionCallStub()
+{
     throw TestException();
 }
 
 // increase call stack depth
-void __attribute__((noinline)) TopCallStub() {
+void __attribute__((noinline)) TopCallStub()
+{
     ThrowExceptionCallStub();
 }
 
@@ -36,9 +55,7 @@ static void Bm_function_throw_exception(benchmark::State &state)
     for (auto _ : state) {
         try {
             TopCallStub();
-        }
-        catch (TestException& e)
-        {
+        } catch (TestException& e) {
             e.check_result();
             ret = gThrowExceptionCnt;
         }
