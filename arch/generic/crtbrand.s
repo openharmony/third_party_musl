5b7a579fcc9257616476f26d46f0e1cacfe8dcc9
yangbinfa <yangbinfa@huawei.com>
2024-08-27 15:37:22 +0800

    修复.so文件中.note.ohos.ident段读取错误问题
    Signed-off-by: yangbinfa <yangbinfa@huawei.com>

--- arch/generic/crtbrand.s
+++ arch/generic/crtbrand.s
@@ -4,9 +4,9 @@
 abitag:
   .long 2f-1f                 // int32_t namesz
   .long 3f-2f                 // int32_t descsz
-  .long 4f-3f
+  .long 1                     // should be the "type" according to the elf spec
 1:.ascii "OHOS\0"          // char name[]
+.balign 4
 2:.long 1  // int32_t ohos_api
 3:
-4:
   .size abitag, .-abitag
