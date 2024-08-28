23d4119404a4c866ab29cc0442141e32212cf70d
ohms9108 <ohos2023@unionman.com.cn>
2024-08-28 11:21:16 +0800

    Implement 13 locale related interfaces in ctype.h
    Signed-off-by: ohms9108 <ohos2023@unionman.com.cn>

--- src/ctype/ispunct.c
+++ src/ctype/ispunct.c
@@ -1,4 +1,8 @@
 #include <ctype.h>
+#ifdef FEATURE_ICU_LOCALE_TMP
+#include <locale.h>
+#include "locale_impl.h"
+#endif
 
 int ispunct(int c)
 {
@@ -7,6 +11,15 @@ int ispunct(int c)
 
 int __ispunct_l(int c, locale_t l)
 {
+#ifdef FEATURE_ICU_LOCALE_TMP
+	if (l && l->cat[LC_CTYPE] && l->cat[LC_CTYPE]->flag == ICU_VALID) {
+		get_icu_symbol(ICU_I18N, &(g_icu_opt_func.u_isgraph), ICU_UCHAR_ISGRAPH_SYMBOL);
+		get_icu_symbol(ICU_I18N, &(g_icu_opt_func.u_isalnum), ICU_UCHAR_ISALNUM_SYMBOL);
+		if (g_icu_opt_func.u_isgraph && g_icu_opt_func.u_isalnum) {
+			return g_icu_opt_func.u_isgraph(c) && !g_icu_opt_func.u_isalnum(c);
+		}
+	}
+#endif
 	return ispunct(c);
 }
 
