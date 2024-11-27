f315787e9451e205d2c613ce65e25c6c02f686bc
liyiming13 <liyiming13@huawei.com>
2024-11-27 17:02:59 +0800

    Fix device_api_version
    Issue:https://gitee.com/openharmony/third_party_musl/issues/IB7GE4?from=project-issue
    Test:libc-test
    
    Signed-off-by: liyiming13 <liyiming13@huawei.com>

--- src/info/linux/device_api_version.c
+++ src/info/linux/device_api_version.c
@@ -13,13 +13,46 @@
  * limitations under the License.
  */
 
+#include <stdlib.h>
+#include <stdio.h>
 #include <info/device_api_version.h>
 
+#ifdef OHOS_ENABLE_PARAMETER
+#include "sys_param.h"
+#endif
+
 #define API_VERSION_DEFAULT 0
+#define API_VERSION_NAME_LEN 256
+
+static int device_api_level = API_VERSION_DEFAULT;
+
+void InitDeviceApiVersion(void)
+{
+#ifdef OHOS_ENABLE_PARAMETER
+    const char para_name[API_VERSION_NAME_LEN] = "const.ohos.apiversion";
+    CachedHandle para_handler = CachedParameterCreate(para_name, "0");
+    if (para_handler == NULL) {
+        device_api_level = API_VERSION_DEFAULT;
+        return;
+    }
+    const char *para_value = CachedParameterGet(para_handler);
+    if (para_value == NULL) {
+        device_api_level = API_VERSION_DEFAULT;
+        return;
+    }
+    int api_level = atoi(para_value);
+    CachedParameterDestroy(para_handler);
+    device_api_level = (api_level > 0 ) ? api_level : API_VERSION_DEFAULT;
+#endif // OHOS_ENABLE_PARAMETER
+}
 
 int get_device_api_version(void)
 {
-    // depend subsystem of syspara support the interface of get system property
-
-    return API_VERSION_DEFAULT;
+#ifdef OHOS_ENABLE_PARAMETER
+    if (device_api_level == API_VERSION_DEFAULT) {
+        // Api version may not init by InitDeviceApiVersion in dls3, try again.
+        InitDeviceApiVersion();
+    }
+#endif // OHOS_ENABLE_PARAMETER
+    return device_api_level;
 }
