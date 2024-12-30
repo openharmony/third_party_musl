955e779f3502005d4df2c9af27104365eaeb3037
wuyuanchao <wuyuanchao4@huawei.com>
2024-12-30 20:07:30 +0800

    add libc-test-cases
    Signed-off-by: wuyuanchao <wuyuanchao4@huawei.com>

--- libc-test/src/ola_test/dlclose/dlclose_12.c
+++ libc-test/src/ola_test/dlclose/dlclose_12.c
@@ -21,8 +21,8 @@
  */
 
 /********************************************************************
- *  @CaseName: dlclose_13
- *  @Brief: 测试dlclose关闭带tls变量的动态库
+ *  @CaseName: dlclose_12
+ *  @Brief: 测试并发创建线程加载卸载动态库
  *  Author: baoruyi
  ********************************************************************/
 #include "common.h" /* PLEASE DON'T MODIFY THIS LINE */
@@ -34,7 +34,7 @@ int tc_pre_test(int argc, char **argv)
     // @PRE-1： 测试so存在, so内容为并发创建线程然后退出
     char *files[] = {
       "/data/tests/libc-test/ola_test/libtest1.so",
-      "/data/tests/libc-test/ola_test/libtest3.so"
+      "/data/tests/libc-test/ola_test/libtest1.so"
     };
     int num_files = sizeof(files) / sizeof(files[0]);
     if (check_so_exist(num_files, files) != 0) {
@@ -43,47 +43,54 @@ int tc_pre_test(int argc, char **argv)
     return RT_EOK;
 }
 
+void *thread_dlopen_dlclose_so(void *arg)
+{
+    int ret = 0;
+    char *direct_libs[] = {
+      "/data/tests/libc-test/ola_test/libtest1.so",
+      "/data/tests/libc-test/ola_test/libtest2.so"
+    };
+    void *direct_hdls = NULL;
+    direct_hdls = dlopen(direct_libs[0], RTLD_NOW | RTLD_GLOBAL);
+    if (!direct_hdls) {
+        printf("dlopen %s failed: %s\n", direct_libs[0], dlerror());
+    }
+    ret = dlclose(direct_hdls);
+    if (ret != 0) {
+        printf("dlclose %s failed: %s\n", direct_libs[0], dlerror());
+    }
+    usleep(UNSLEEP_TIME);
+    return NULL;
+}
+
 // Func: 用例执行测试活动，重要：本函数必须定义且函数名不可修改
 // Args: 同用例参数
 int tc_do_test(int argc, char **argv)
 {
-    // @STEP-1: dlopen加载libtest3.so
-    // @EXPECT-1: 2个so都加载成功
+    // @STEP-1: 并发1000次加载卸载动态库
+    // @EXPECT-1: 执行成功
     int ret = 0;
-    char *direct_libs[] = {
-        "/data/tests/libc-test/ola_test/libtest3.so",
-        "/data/tests/libc-test/ola_test/libtest1.so"
-    };
-    void *direct_hdls = NULL;
+    pthread_t thid[TEST_NUM];
+    char *direct_libs[] = { "/data/tests/libc-test/ola_test/libtest1.so", "/data/tests/libc-test/ola_test/libtest2.so" };
     int num = sizeof(direct_libs) / sizeof(direct_libs[0]);
-    dirrect_hdls = dlopen(direct_libs[0], RTLD_NOW | RTLD_GLOBAL);
-    if (!direct_hdls) {
-        rt_err("dlopen %s failed: %s\n", direct_libs[0], dlerror());
-    }
-    for (int i = 0; i < num; i++) {
-        if (!check_loaded(direct_libs[i])) {
-        rt_err("dso %s shouldn't exist in /proc/self/maps\n", direct_libs[i]);
+    for (int k = 0; k < TEST_NUM; k++) {
+        ret = pthread_create(&thid[k], NULL, (void *)thread_dlopen_dlclose_so, NULL);
+        if (ret != 0) {
+            printf("creat pthread failed errno is %s\n", strerror(errno));
         }
     }
-    // @STEP-2: 卸载libtest3.so
-    // @EXPECT-2：libtest3.so无法被卸载
-    ret = dlclose(direct_hdls);
-    if (ret != 0) {
-        rt_err("dlclose %s failed: %s\n", direct_libs[0], dlerror());
+    for (int j = 0; j < TEST_NUM; j++) {
+        ret = pthread_join(tid[j], NULL);
+        if (ret != 0) {
+            printf("pthread_join fail errno is %s\n", strerror(errno));
+        }
     }
-    if (!check_loaded(direct_libs[0])) {
-        rt_err("dso %s shouldn't exist in /proc/self/maps\n", direct_libs[0]);
+    for (int i = 0; i < num; i++) {
+        if (check_loaded(direct_libs[i])) {
+            printf("dso %s shouldn't exist in /proc/self/maps\n", direct_libs[i]);
+        }
     }
-    if (!check_loaded(direct_libs[1])) {
-        rt_err("dso %s shouldn't exist in /proc/self/maps\n", direct_libs[1]);
-    }
-    // @STEP-3: dlsym libtest3.so
-    // @EXPECT-3: 结果为空
-    void *res;
-    res = dlsym(direct_hdls, direct_libs[0]);
-    if (res != NULL) {
-        rt_err("dlsym %s failed: %s\n", direct_libs[0], dlerror());
-    }
+
     return RT_EOK;
 }
 
@@ -102,20 +109,20 @@ int tc_get_debuginfo(int argc, char **argv)
 
 int main() {
     if (tc_pre_test(0, NULL)) {
-        rt_err("tc_pre_test failed:\n");
-        return -1;
+      rt_err("tc_pre_test failed:\n");
+      return -1;
     }
     if (tc_do_test(0, NULL)) {
-        rt_err("tc_do_test failed:\n");
-        return -1;
+      rt_err("tc_do_test failed:\n");
+      return -1;
     }
     if (tc_post_test(0, NULL)) {
-        rt_err("tc_post_test failed:\n");
-        return -1;
+      rt_err("tc_post_test failed:\n");
+      return -1;
     }
     if (tc_get_debuginfo(0, NULL)) {
-        rt_err("tc_get_debuginfo failed:\n");
-        return -1;
+      rt_err("tc_get_debuginfo failed:\n");
+      return -1;
     }
     return 0;
 }
