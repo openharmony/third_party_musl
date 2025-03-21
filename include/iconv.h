a7855067dc50a067a37de99b73a91ca61f924968
yedexi <yedexi1@huawei.com>
2025-03-21 09:43:33 +0000

    update include/iconv.h.
    Signed-off-by: yedexi <yedexi1@huawei.com>

--- include/iconv.h
+++ include/iconv.h
@@ -13,7 +13,7 @@ extern "C" {
 
 typedef void *iconv_t;
 
-int set_iconv_icu_enable();
+int set_iconv_icu_enable(void);
 iconv_t iconv_open(const char *, const char *);
 size_t iconv(iconv_t, char **__restrict, size_t *__restrict, char **__restrict, size_t *__restrict);
 int iconv_close(iconv_t);
