a2f9e1a5a29db1cd7139f8ab8d36f0d9f4f1a158
qiu_yu_jia <qiuyujia1@huawei.com>
2025-03-21 15:43:18 +0800

    Roll back code from 5.1 branch
    Signed-off-by: qiu_yu_jia <qiuyujia1@huawei.com>

--- src/ctype/isspace.c
+++ src/ctype/isspace.c
@@ -1,9 +1,5 @@
 #include <ctype.h>
 #undef isspace
-#ifdef FEATURE_ICU_LOCALE_TMP
-#include <locale.h>
-#include "locale_impl.h"
-#endif
 
 int isspace(int c)
 {
@@ -12,14 +8,6 @@ int isspace(int c)
 
 int __isspace_l(int c, locale_t l)
 {
-#ifdef FEATURE_ICU_LOCALE_TMP
-	if (l && l->cat[LC_CTYPE] && l->cat[LC_CTYPE]->flag == ICU_VALID) {
-		get_icu_symbol(ICU_I18N, &(g_icu_opt_func.u_isspace), ICU_UCHAR_ISSPACE_SYMBOL);
-		if (g_icu_opt_func.u_isspace) {
-			return g_icu_opt_func.u_isspace(c);
-		}
-	}
-#endif
 	return isspace(c);
 }
 
