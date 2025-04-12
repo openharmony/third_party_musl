23f562e8b7c42cda4857f96f87c86b54c440005a
zyxzyx <zhangyixin19@huawei.com>
2025-04-12 11:36:10 +0800

    fix signal 36 unsafe operations
    Signed-off-by: zyxzyx <zhangyixin19@huawei.com>

--- porting/linux/user/src/hook/musl_preinit.c
+++ porting/linux/user/src/hook/musl_preinit.c
@@ -559,12 +559,7 @@ static void __install_malloc_hook()
 		}
 	} else if (shared_library_handle != (void*)-1) {
 		__restore_hook_function_table();
-		on_start_func_t start_func = (on_start_func_t)(function_of_shared_lib[ON_START_FUNCTION]);
-		if (start_func && !start_func(__uninstal_malloc_hook)) {
-			// __musl_log(__MUSL_LOG_ERROR, "%s: failed to enable malloc\n", getprogname());
-		}
-		volatile const struct MallocDispatchType* so_dispatch_value = (volatile const struct MallocDispatchType* )atomic_load_explicit(&__musl_libc_globals.so_dispatch_table, memory_order_acquire);
-		atomic_store_explicit(&__musl_libc_globals.current_dispatch_table, (volatile long long)so_dispatch_value, memory_order_seq_cst);
+		atomic_store_explicit(&__musl_libc_globals.current_dispatch_table, (volatile const long long)&__ohos_malloc_hook_init_dispatch, memory_order_seq_cst);
 	}
 }
 
