c23dae3e950d7cdab1ec96d7babfa855c53f5f55
xwx1135370 <xuyao44@huawei.com>
2025-05-26 11:16:09 +0800

    Add mkdtemp and mktemp test
    issue:https://gitee.com/openharmony/third_party_musl/issues/IC9N4T?from=project-issue
    
    Signed-off-by: xwx1135370 <xuyao44@huawei.com>

--- libc-test/src/functionalext/supplement/temp/mktemp.c
+++ libc-test/src/functionalext/supplement/temp/mktemp.c
@@ -61,10 +61,68 @@ void mktemp_0300(void)
     }
 }
 
+/**
+ * @tc.name      : mktemp_0400
+ * @tc.desc      : Create temp file with template argument （'XXXXXX' not at the end）
+ * @tc.level     : Level 2
+ */
+void mktemp_0400(void)
+{
+    char temp_dir[] = "test-XXXXXX-dir";
+    char *ret = mktemp(temp_dir);
+    EXPECT_PTRNE("mktemp_0400", ret, NULL);
+    //XXXXXX is not at the end, mktemp() will fail
+    if (ret) {
+        EXPECT_EQ("mktemp_0400", errno, EINVAL);
+    }
+}
+
+/**
+ * @tc.name      : mktemp_0500
+ * @tc.desc      : Repeated calls generate unique directories
+ * @tc.level     : Level 1
+ */
+void mktemp_0500(void)
+{
+    char temp_dir1[] = "test-XXXXXX";
+    char temp_dir2[] = "test-XXXXXX";
+    char *ret1 = mkdtemp(temp_dir1);
+    char *ret2 = mkdtemp(temp_dir2);
+    EXPECT_PTRNE("mktemp_0500", ret1, NULL);
+    EXPECT_PTRNE("mktemp_0500", ret2, NULL);
+    if (ret1 && ret2) {
+        EXPECT_STRNE("mktemp_0500", ret1, ret2); // The two results should be different
+    }
+}
+
+/**
+ * @tc.name      : mktemp_0600
+ * @tc.desc      : Verify mktemp retries when file exists
+ * @tc.level     : Level 1
+ */
+void mktemp_0600(void)
+{
+    char temp_file[] = "test-XXXXXX";
+
+    int fd = mkstemp(temp_file); // create file
+
+    if (fd != -1)  {
+        char *ret = mktemp(temp_file);
+        EXPECT_PTRNE("mktemp_0600", ret, NULL);
+        if(ret) {
+            EXPECT_EQ("mktemp_0600", ret[0], 0);
+        }
+        unlink(temp_file); // clean file
+    }
+}
+
 int main(void)
 {
     mktemp_0100();
     mktemp_0200();
     mktemp_0300();
+    mktemp_0400();
+    mktemp_0500();
+    mktemp_0600();
     return t_status;
 }
