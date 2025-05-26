8b96a15366ed89beec365a44e678325985755fdc
xwx1135370 <xuyao44@huawei.com>
2025-05-26 11:16:09 +0800

    Add mkdtemp and mktemp test
    issue:https://gitee.com/openharmony/third_party_musl/issues/IC9N4T?from=project-issue
    
    Signed-off-by: xwx1135370 <xuyao44@huawei.com>

--- libc-test/src/functionalext/supplement/temp/mkdtemp.c
+++ libc-test/src/functionalext/supplement/temp/mkdtemp.c
@@ -14,6 +14,7 @@
  */
 
 #include <stdlib.h>
+#include <sys/stat.h>
 #include "functionalext.h"
 
 /**
@@ -55,10 +56,65 @@ void mkdtemp_0300(void)
     EXPECT_PTREQ("mkdtemp_0300", ret, NULL);
 }
 
+/**
+ * @tc.name      : mkdtemp_0400
+ * @tc.desc      : Template with 'XXXXXX' not at the end
+ * @tc.level     : Level 2
+ */
+void mkdtemp_0400(void)
+{
+    char temp_dir[] = "test-XXXXXX-dir";
+    char *ret = mkdtemp(temp_dir);
+    //XXXXXX is not at the end, mkdtemp() will fail and return NULL. Now template is unchanged.
+    EXPECT_PTREQ("mkdtemp_0400", ret, NULL);
+    if (ret == NULL) {
+        EXPECT_EQ("mkdtemp_0400", errno, EINVAL);
+        EXPECT_STREQ("mkdtemp_0400", temp_dir, "test-XXXXXX-dir");
+    }
+}
+
+/**
+ * @tc.name      : mkdtemp_0500
+ * @tc.desc      : Repeated calls generate unique directories
+ * @tc.level     : Level 1
+ */
+void mkdtemp_0500(void)
+{
+    char temp_dir1[] = "test-XXXXXX";
+    char temp_dir2[] = "test-XXXXXX";
+    char *ret1 = mkdtemp(temp_dir1);
+    char *ret2 = mkdtemp(temp_dir2);
+    EXPECT_PTRNE("mkdtemp_0500", ret1, NULL);
+    EXPECT_PTRNE("mkdtemp_0500", ret2, NULL);
+    if (ret1 && ret2) {
+        EXPECT_STRNE("mkdtemp_0500", ret1, ret2); // The two results should be different
+    }
+}
+
+/**
+ * @tc.name      : mkdtemp_0600
+ * @tc.desc      : Check directory permissions (should be 0700)
+ * @tc.level     : Level 2
+ */
+void mkdtemp_0600(void) {
+    char temp_dir[] = "test-XXXXXX";
+    char *ret = mkdtemp(temp_dir);
+    EXPECT_PTRNE("mkdtemp_0600", ret, NULL);
+    if (ret) {
+        struct stat st;
+        stat(ret, &st);
+        EXPECT_EQ("mkdtemp_0600", st.st_mode & 0777, 0700);
+        rmdir(ret);
+    }
+}
+
 int main(void)
 {
     mkdtemp_0100();
     mkdtemp_0200();
     mkdtemp_0300();
+    mkdtemp_0400();
+    mkdtemp_0500();
+    mkdtemp_0600();
     return t_status;
 }
