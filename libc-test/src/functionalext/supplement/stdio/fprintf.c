94c5e2fe1b34a226e48d86a1aee939ce0fb78e9c
wuyuanchao <wuyuanchao4@huawei.com>
2025-06-20 17:32:38 +0800

    Bugfix:Add missing code tests
    Issue: https://gitee.com/openharmony/third_party_musl/issues/ICGPZW
    
    Signed-off-by: wuyuanchao <wuyuanchao4@huawei.com>

--- libc-test/src/functionalext/supplement/stdio/fprintf.c
+++ libc-test/src/functionalext/supplement/stdio/fprintf.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 Huawei Device Co., Ltd.
+ * Copyright (C) 2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -18,7 +18,7 @@
 #include <stdlib.h>
 #include "functionalext.h"
 
-const int32_t ERRORLEN = -1;
+const size_t ERRORLEN = -1;
 
 /**
  * @tc.name:      fprintf_0100
@@ -28,7 +28,7 @@ const int32_t ERRORLEN = -1;
 void fprintf_0100(void)
 {
     char str[] = "This is a fprintf_0100";
-    int32_t len = fprintf(stdout, "%s", str);
+    size_t len = fprintf(stdout, "%s", str);
     EXPECT_EQ("fprintf_0100", len, strlen(str));
 }
 
@@ -40,7 +40,7 @@ void fprintf_0100(void)
 void fprintf_0200(void)
 {
     char str[] = "This is a test fprintf_0200";
-    int32_t len = fprintf(stderr, "%s", str);
+    size_t len = fprintf(stderr, "%s", str);
     EXPECT_EQ("fprintf_0200", len, strlen(str));
 }
 
@@ -69,7 +69,7 @@ void fprintf_0300(void)
 void fprintf_0400(void)
 {
     char str[] = "这是一个测试";
-    int32_t len = 0;
+    size_t len = 0;
     FILE *fptr = fopen("tempory_testfprintf.txt", "r");
     if (fptr != NULL) {
         len = fprintf(fptr, "%s", str);
@@ -79,11 +79,107 @@ void fprintf_0400(void)
     remove("tempory_testfprintf.txt");
 }
 
+/**
+ * @tc.name:      fprintf_0500
+ * @tc.desc:      Verify fprintf with multiple format specifiers.
+ * @tc.level:     level 0.
+ */
+void fprintf_0500(void)
+{
+    char str[] = "Test";
+    int num = 123;
+    double d = 45.67;
+    size_t len = 0;
+    FILE *fptr = fopen("tempory_testfprintf.txt", "w");
+    if (fptr != NULL) {
+        len = fprintf(fptr, "String: %s, Int: %d, Double: %.2f", str, num, d);
+        fclose(fptr);
+    }
+    int expected_len = strlen("String: Test, Int: 123, Double: 45.67");
+    EXPECT_EQ("fprintf_0500", len, expected_len);
+    remove("tempory_testfprintf.txt");
+}
+
+
+/**
+ * @tc.name:      fprintf_0600
+ * @tc.desc:      Verify fprintf with append mode.
+ * @tc.level:     level 0.
+ */
+void fprintf_0600(void)
+{
+    char str1[] = "First line\n";
+    char str2[] = "Second line\n";
+    
+    // Write first line
+    FILE *fptr = fopen("tempory_testfprintf.txt", "w");
+    if (fptr != NULL) {
+        fprintf(fptr, "%s", str1);
+        fclose(fptr);
+    }
+    
+    // Append second line
+    fptr = fopen("tempory_testfprintf.txt", "a");
+    size_t len = 0;
+    if (fptr != NULL) {
+        len = fprintf(fptr, "%s", str2);
+        fclose(fptr);
+    }
+    
+    EXPECT_EQ("fprintf_0600", len, strlen(str2));
+    remove("tempory_testfprintf.txt");
+}
+
+/**
+ * @tc.name:      fprintf_0700
+ * @tc.desc:      Verify fprintf with large string.
+ * @tc.level:     level 0.
+ */
+void fprintf_0700(void)
+{
+    char large_str[1024];
+    memset(large_str, 'A', sizeof(large_str)-1);
+    large_str[sizeof(large_str)-1] = '\0';
+    
+    size_t len = 0;
+    FILE *fptr = fopen("tempory_testfprintf.txt", "w");
+    if (fptr != NULL) {
+        len = fprintf(fptr, "%s", large_str);
+        fclose(fptr);
+    }
+    
+    EXPECT_EQ("fprintf_0700", len, strlen(large_str));
+    remove("tempory_testfprintf.txt");
+}
+
+/**
+ * @tc.name:      fprintf_0800
+ * @tc.desc:      Verify fprintf with special characters.
+ * @tc.level:     level 0.
+ */
+void fprintf_0800(void)
+{
+    char str[] = "Special chars: \t\n\r\\\"\'";
+    size_t len = 0;
+    FILE *fptr = fopen("tempory_testfprintf.txt", "w");
+    if (fptr != NULL) {
+        len = fprintf(fptr, "%s", str);
+        fclose(fptr);
+    }
+    
+    EXPECT_EQ("fprintf_0800", len, strlen(str));
+    remove("tempory_testfprintf.txt");
+}
+
 int main(void)
 {
     fprintf_0100();
     fprintf_0200();
     fprintf_0300();
     fprintf_0400();
+    fprintf_0500();
+    fprintf_0600();
+    fprintf_0700();
+    fprintf_0800();
     return t_status;
 }
