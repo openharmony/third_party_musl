3d2e4dfb70a1f1910631f749de268b1104415cfb
wujianlin_1 <wujianlin8@h-partners.com>
2025-09-25 21:28:39 +0800

    Add the libc_test test case
    Issue:https://gitcode.com/openharmony/third_party_musl/issues/1732
    Signed-off-by: wujianlin_1 <wujianlin8@h-partners.com>

--- libc-test/src/functionalext/supplement/ctype/iscntrl.c
+++ libc-test/src/functionalext/supplement/ctype/iscntrl.c
@@ -81,6 +81,83 @@ void iscntrl_0500(void)
     EXPECT_EQ("iscntrl_0500", total, COUNT);
 }
 
+/**
+ * @tc.name      : iscntrl_0600
+ * @tc.desc      : If the parameter c is 0x20 (32, space), it is judged as a non-control character.
+ * @tc.level     : Level 0
+ */
+void iscntrl_0600(void)
+{
+    int ret = iscntrl(0x20);
+    EXPECT_EQ("iscntrl_0600", ret, RET);
+}
+
+/**
+ * @tc.name      : iscntrl_0700
+ * @tc.desc      : If the parameter c is 0x7E (126, '~'), it is judged as a non-control character.
+ * @tc.level     : Level 0
+ */
+void iscntrl_0700(void)
+{
+    int ret = iscntrl(0x7E);
+    EXPECT_EQ("iscntrl_0700", ret, RET);
+}
+
+/**
+ * @tc.name      : iscntrl_0800
+ * @tc.desc      : If the parameter c is 0x0A ('\n'), it is judged as a control character.
+ * @tc.level     : Level 1
+ */
+void iscntrl_0800(void)
+{
+    int ret = iscntrl('\n');
+    EXPECT_NE("iscntrl_0800", ret, RET);
+}
+
+/**
+ * @tc.name      : iscntrl_0900
+ * @tc.desc      : If the parameter c is 0x09 ('\t'), it is judged as a control character.
+ * @tc.level     : Level 1
+ */
+void iscntrl_0900(void)
+{
+    int ret = iscntrl('\t');
+    EXPECT_NE("iscntrl_0900", ret, RET);
+}
+
+/**
+ * @tc.name      : iscntrl_1000
+ * @tc.desc      : If the parameter c is 0x0D ('\r'), it is judged as a control character.
+ * @tc.level     : Level 1
+ */
+void iscntrl_1000(void)
+{
+    int ret = iscntrl('\r');
+    EXPECT_NE("iscntrl_1000", ret, RET);
+}
+
+/**
+ * @tc.name      : iscntrl_1100
+ * @tc.desc      : If the parameter c is 0x80 (128, non-ASCII), it is judged as a non-control character.
+ * @tc.level     : Level 1
+ */
+void iscntrl_1100(void)
+{
+    int ret = iscntrl(0x80);
+    EXPECT_EQ("iscntrl_1100", ret, RET);
+}
+
+/**
+ * @tc.name      : iscntrl_1200
+ * @tc.desc      : If the parameter c is -1 (negative), it is judged as a non-control character.
+ * @tc.level     : Level 2
+ */
+void iscntrl_1200(void)
+{
+    int ret = iscntrl(-1);
+    EXPECT_EQ("iscntrl_1200", ret, RET);
+}
+
 int main(void)
 {
     iscntrl_0100();
@@ -88,5 +165,12 @@ int main(void)
     iscntrl_0300();
     iscntrl_0400();
     iscntrl_0500();
+    iscntrl_0600();
+    iscntrl_0700();
+    iscntrl_0800();
+    iscntrl_0900();
+    iscntrl_1000();
+    iscntrl_1100();
+    iscntrl_1200();
     return t_status;
 }
