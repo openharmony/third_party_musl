d3b154156678f806d24671e78cae96c4c267c7e8
xwx1135370 <xuyao44@huawei.com>
2025-10-09 13:51:50 +0800

    Add string test case
    issue:https://gitcode.com/openharmony/third_party_musl/pull/2216
    
    Signed-off-by: xwx1135370 <xuyao44@huawei.com>

--- libc-test/src/functionalext/supplement/string/wcstok.c
+++ libc-test/src/functionalext/supplement/string/wcstok.c
@@ -64,10 +64,203 @@ void wcstok_0200(void)
     }
 }
 
+/**
+ * @tc.name      : wcstok_0300
+ * @tc.desc      : Test the wcstok function with consecutive delimiters
+ * @tc.level     : Level 0
+ */
+void wcstok_0300(void)
+{
+    wchar_t str[] = L",,hello,,world,,test,,";
+    wchar_t *saveptr = NULL;
+    wchar_t *result;
+    const wchar_t *delim = L",";
+
+    result = wcstok(str, delim, &saveptr);
+    if (wcscmp(result, L"hello") != 0) {
+        t_error("%s wcstok get first result is %ls are not want 'hello'\n", __func__, result);
+        return;
+    }
+
+    result = wcstok(NULL, delim, &saveptr);
+    if (wcscmp(result, L"world") != 0) {
+        t_error("%s wcstok get second result is %ls are not want 'world'\n", __func__, result);
+        return;
+    }
+
+    result = wcstok(NULL, delim, &saveptr);
+    if (wcscmp(result, L"test") != 0) {
+        t_error("%s wcstok get third result is %ls are not want 'test'\n", __func__, result);
+        return;
+    }
+
+    result = wcstok(NULL, delim, &saveptr);
+    if (result != NULL) {
+        t_error("%s wcstok should return NULL but get %ls\n", __func__, result);
+    }
+}
+
+/**
+ * @tc.name      : wcstok_0400
+ * @tc.desc      : Test the wcstok function with empty string
+ * @tc.level     : Level 0
+ */
+void wcstok_0400(void)
+{
+    wchar_t str[] = L"";
+    wchar_t *saveptr = NULL;
+    wchar_t *result;
+    const wchar_t *delim = L",";
+
+    result = wcstok(str, delim, &saveptr);
+    if (result != NULL) {
+        t_error("%s wcstok should return NULL for empty string but get %ls\n", __func__, result);
+    }
+}
+
+/**
+ * @tc.name      : wcstok_0500
+ * @tc.desc      : Test the wcstok function with delimiter only string
+ * @tc.level     : Level 0
+ */
+void wcstok_0500(void)
+{
+    wchar_t str[] = L",,,,";
+    wchar_t *saveptr = NULL;
+    wchar_t *result;
+    const wchar_t *delim = L",";
+
+    result = wcstok(str, delim, &saveptr);
+    if (result != NULL) {
+        t_error("%s wcstok should return NULL for delimiter only string but get %ls\n", __func__, result);
+    }
+}
+
+/**
+ * @tc.name      : wcstok_0600
+ * @tc.desc      : Test the wcstok function with no delimiters in string
+ * @tc.level     : Level 0
+ */
+void wcstok_0600(void)
+{
+    wchar_t str[] = L"helloworld";
+    wchar_t *saveptr = NULL;
+    wchar_t *result;
+    const wchar_t *delim = L",";
+
+    result = wcstok(str, delim, &saveptr);
+    if (wcscmp(result, L"helloworld") != 0) {
+        t_error("%s wcstok get result is %ls are not want 'helloworld'\n", __func__, result);
+        return;
+    }
+
+    result = wcstok(NULL, delim, &saveptr);
+    if (result != NULL) {
+        t_error("%s wcstok should return NULL but get %ls\n", __func__, result);
+    }
+}
+
+/**
+ * @tc.name      : wcstok_0700
+ * @tc.desc      : Test the wcstok function thread safety with multiple save pointers
+ * @tc.level     : Level 1
+ */
+void wcstok_0700(void)
+{
+    wchar_t str1[] = L"apple,banana,cherry";
+    wchar_t str2[] = L"red;green;blue";
+    wchar_t *saveptr1 = NULL;
+    wchar_t *saveptr2 = NULL;
+    wchar_t *result1, *result2;
+
+    // Process both strings alternately
+    result1 = wcstok(str1, L",", &saveptr1);
+    result2 = wcstok(str2, L";", &saveptr2);
+
+    if (wcscmp(result1, L"apple") != 0) {
+        t_error("%s wcstok get first result1 is %ls are not want 'apple'\n", __func__, result1);
+        return;
+    }
+
+    if (wcscmp(result2, L"red") != 0) {
+        t_error("%s wcstok get first result2 is %ls are not want 'red'\n", __func__, result2);
+        return;
+    }
+
+    // Continue processing
+    result1 = wcstok(NULL, L",", &saveptr1);
+    result2 = wcstok(NULL, L";", &saveptr2);
+
+    if (wcscmp(result1, L"banana") != 0) {
+        t_error("%s wcstok get second result1 is %ls are not want 'banana'\n", __func__, result1);
+        return;
+    }
+
+    if (wcscmp(result2, L"green") != 0) {
+        t_error("%s wcstok get second result2 is %ls are not want 'green'\n", __func__, result2);
+        return;
+    }
+}
+
+/**
+ * @tc.name      : wcstok_0800
+ * @tc.desc      : Test the wcstok function with NULL input
+ * @tc.level     : Level 2
+ */
+void wcstok_0800(void)
+{
+    wchar_t *saveptr = NULL;
+    wchar_t *result;
+    const wchar_t *delim = L",";
+
+    result = wcstok(NULL, delim, &saveptr);
+    if (result != NULL) {
+        t_error("%s wcstok should return NULL for NULL input but get %ls\n", __func__, result);
+    }
+}
+
+/**
+ * @tc.name      : wcstok_0900
+ * @tc.desc      : Test the wcstok function with mixed delimiters and whitespace
+ * @tc.level     : Level 1
+ */
+void wcstok_0900(void)
+{
+    wchar_t str[] = L"  hello , world ; test ";
+    wchar_t *saveptr = NULL;
+    wchar_t *result;
+    const wchar_t *delim = L" ,;";
+
+    result = wcstok(str, delim, &saveptr);
+    if (wcscmp(result, L"hello") != 0) {
+        t_error("%s wcstok get first result is %ls are not want 'hello'\n", __func__, result);
+        return;
+    }
+
+    result = wcstok(NULL, delim, &saveptr);
+    if (wcscmp(result, L"world") != 0) {
+        t_error("%s wcstok get second result is %ls are not want 'world'\n", __func__, result);
+        return;
+    }
+
+    result = wcstok(NULL, delim, &saveptr);
+    if (wcscmp(result, L"test") != 0) {
+        t_error("%s wcstok get third result is %ls are not want 'test'\n", __func__, result);
+        return;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     setlocale(LC_ALL, "en_US.utf8");
     wcstok_0100();
     wcstok_0200();
+    wcstok_0300();
+    wcstok_0400();
+    wcstok_0500();
+    wcstok_0600();
+    wcstok_0700();
+    wcstok_0800();
+    wcstok_0900();
     return t_status;
 }
