44d4afe483bbca454c759a54224a14970de6a09e
wujianlin_1 <wujianlin8@h-partners.com>
2025-10-27 21:16:36 +0800

    Supplement libc_test string test cases
    Issue:https://gitcode.com/openharmony/third_party_musl/issues/1745
    Signed-off-by: wujianlin_1 <wujianlin8@h-partners.com>

--- libc-test/src/functionalext/supplement/string/wcslen.c
+++ libc-test/src/functionalext/supplement/string/wcslen.c
@@ -28,12 +28,125 @@ void wcslen_0100(void)
     wchar_t str[] = L"wcslen";
     int target = 6;
     size_t num = wcslen(str);
-    if (num != target)
+    if (num != target) {
         t_error("%s wcslen is not right", __func__);
+    }
+}
+
+/**
+ * @tc.name      : wcslen_0200
+ * @tc.desc      : Calculate the length of an empty wide character string
+ * @tc.level     : Level 1
+ */
+void wcslen_0200(void)
+{
+    wchar_t str[] = L"";
+    int target = 0;
+    size_t num = wcslen(str);
+    if (num != target) {
+        t_error("%s empty string wcslen is not right", __func__);
+    }
+}
+
+/**
+ * @tc.name      : wcslen_0300
+ * @tc.desc      : Calculate the length of a wide character string with spaces
+ * @tc.level     : Level 1
+ */
+void wcslen_0300(void)
+{
+    wchar_t str[] = L"Hello World";
+    int target = 11;
+    size_t num = wcslen(str);
+    if (num != target) {
+        t_error("%s string with spaces wcslen is not right", __func__);
+    }
+}
+
+/**
+ * @tc.name      : wcslen_0400
+ * @tc.desc      : Calculate the length of a wide character string with special characters
+ * @tc.level     : Level 1
+ */
+void wcslen_0400(void)
+{
+    wchar_t str[] = L"Test@123#";
+    int target = 9;
+    size_t num = wcslen(str);
+    if (num != target) {
+        t_error("%s special characters wcslen is not right", __func__);
+    }
+}
+
+/**
+ * @tc.name      : wcslen_0500
+ * @tc.desc      : Calculate the length of a wide character string with unicode characters
+ * @tc.level     : Level 2
+ */
+void wcslen_0500(void)
+{
+    wchar_t str[] = L"中文测试";
+    int target = 4;
+    size_t num = wcslen(str);
+    if (num != target) {
+        t_error("%s unicode characters wcslen is not right", __func__);
+    }
+}
+
+/**
+ * @tc.name      : wcslen_0600
+ * @tc.desc      : Calculate the length of a single wide character
+ * @tc.level     : Level 1
+ */
+void wcslen_0600(void)
+{
+    wchar_t str[] = L"A";
+    int target = 1;
+    size_t num = wcslen(str);
+    if (num != target) {
+        t_error("%s single character wcslen is not right", __func__);
+    }
+}
+
+/**
+ * @tc.name      : wcslen_0700
+ * @tc.desc      : Calculate the length of a wide character string with mixed languages
+ * @tc.level     : Level 2
+ */
+void wcslen_0700(void)
+{
+    wchar_t str[] = L"Hello 世界 123";
+    int target = 12;
+    size_t num = wcslen(str);
+    if (num != target) {
+        t_error("%s mixed languages wcslen is not right", __func__);
+    }
+}
+
+/**
+ * @tc.name      : wcslen_0800
+ * @tc.desc      : Calculate the length of a wide character string with null character in middle
+ * @tc.level     : Level 2
+ */
+void wcslen_0800(void)
+{
+    wchar_t str[] = L"Partial\0String";
+    int target = 7; // Should stop at null character
+    size_t num = wcslen(str);
+    if (num != target) {
+        t_error("%s null character wcslen is not right", __func__);
+    }
 }
 
 int main(int argc, char *argv[])
 {
     wcslen_0100();
+    wcslen_0200();
+    wcslen_0300();
+    wcslen_0400();
+    wcslen_0500();
+    wcslen_0600();
+    wcslen_0700();
+    wcslen_0800();
     return t_status;
 }
