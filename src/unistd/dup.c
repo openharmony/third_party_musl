285e909f9939d484af2504ffcec7cb447ec76d86
codeGJT <gejintao@huawei.com>
2025-11-25 22:25:14 +0800

    update: 更新文件 dup.c
    Signed-off-by: codeGJT <gejintao@huawei.com>

--- src/unistd/dup.c
+++ src/unistd/dup.c
@@ -8,9 +8,10 @@
 int dup(int fd)
 {
 #ifdef OHOS_FDTRACK_HOOK_ENABLE
+	restrace(RES_FD_DUP, fd, FD_SIZE, TAG_RES_FD_DUP, true);
 	int fd_new = syscall(SYS_dup, fd);
 	int res = FDTRACK_START_HOOK(fd_new);
-	restrace(RES_FD_DUP, res, FD_SIZE, TAG_RES_FD_DUP, true);
+	restrace(RES_DMABUF_MASK, res, FD_SIZE, TAG_RES_DMABUF_MASK, true);
 	return res;
 #endif
 	return syscall(SYS_dup, fd);
