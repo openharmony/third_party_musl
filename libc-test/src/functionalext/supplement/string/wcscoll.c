c2dce20f2876ce1221236701edf00e82225f9f69
jichunjiang-30079441 <jichunjiang1@h-partners.com>
2026-01-27 17:40:24 +0800

    add wcsrtombs wcscoll test case
    Signed-off-by: jichunjiang-30079441 <jichunjiang1@h-partners.com>

--- libc-test/src/functionalext/supplement/string/wcscoll.c
+++ libc-test/src/functionalext/supplement/string/wcscoll.c
@@ -583,46 +583,6 @@ void wcscoll_2800(void)
     }
 }
 
-/**
- * @tc.name      : wcscoll_2900
- * @tc.desc      : Test wcscoll with strings containing only digits
- * @tc.level     : Level 1
- */
-void wcscoll_2900(void)
-{
-    wchar_t *str1 = L"123";
-    wchar_t *str2 = L"456";
-    wchar_t *str3 = L"123";
-
-    int result1 = wcscoll(str1, str2);
-    if (result1 >= 0) {
-        t_error("%s: wcscoll returns %d (expected negative for '123' < '456')", __func__, result1);
-    }
-
-    int result2 = wcscoll(str1, str3);
-    if (result2 != 0) {
-        t_error("%s: wcscoll returns %d (expected 0 for equal digit strings)", __func__, result2);
-    }
-}
-
-/**
- * @tc.name      : wcscoll_3000
- * @tc.desc      : Test wcscoll with mixed locale-aware comparison
- * @tc.level     : Level 2
- */
-void wcscoll_3000(void)
-{
-    wchar_t *str1 = L"cooperate";
-    wchar_t *str2 = L"co-operate";
-
-    int result = wcscoll(str1, str2);
-    int result2 = wcscoll(str2, str1);
-
-    if (!((result < 0 && result2 > 0) || (result > 0 && result2 < 0) || (result == 0 && result2 == 0))) {
-        t_error("%s: inconsistent results (result1=%d, result2=%d) for hyphenated words", __func__, result, result2);
-    }
-}
-
 int main(int argc, char *argv[])
 {
     wcscoll_0100();
@@ -653,8 +613,6 @@ int main(int argc, char *argv[])
     wcscoll_2600();
     wcscoll_2700();
     wcscoll_2800();
-    wcscoll_2900();
-    wcscoll_3000();
     return t_status;
 }
 
