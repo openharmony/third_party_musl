71ca606ba4c33dd60356bc3252de9924e7bf9e98
oh_hw_llm <liu.limin@huawei.com>
2026-06-03 22:14:11 +0800

    lite ndk打桩so生成
    Signed-off-by: oh_hw_llm <liu.limin@huawei.com>

---
+++ src/riscv32_temp.c
@@ -0,0 +1,51 @@
+void update_register_count(void){}
+
+void ___errno_location(void){}
+
+void *__libc_calloc(void){}
+
+void *__dl_vseterr(void){}
+
+void __tl_lock(void){}
+
+void get_tl_lock_caller_count(void){}
+
+void __membarrier(void){}
+void __tl_unlock(void){}
+void setDlcloseLockStatus(void){}
+void setDlcloseLockLastExitTid(void){}
+void pthread_reserve_signal_stack(void){}
+void __dl_seterr(void){}
+void __synccall(void){}
+void __getopt_msg(void){}
+void __lockfile(void){}
+void __unlockfile(void){}
+void __res_msend(void){}
+void res_msend_rc_ext(void){}
+void __register_locked_file(void){}
+void __randname(void){}
+void __private_cond_signal(void){}
+void __pthread_cond_timedwait(void){}
+void __pthread_mutex_timedlock(void){}
+void __pthread_mutex_trylock(void){}
+void __pthread_mutex_unlock(void){}
+void __pthread_create(void){}
+
+void __pthread_exit(void){}
+void __pthread_join(void){}
+void __pthread_key_create(void){}
+void __pthread_key_delete(void){}
+void __asctime_r(void){}
+void get_tl_lock_count(void){}
+void get_tl_lock_waiters(void){}
+void get_tl_lock_tid_fail(void){}
+void get_tl_lock_count_tid(void){}
+void get_tl_lock_count_fail(void){}
+void get_tl_lock_count_tid_sub(void){}
+void get_thread_list_lock_after_lock(void){}
+void get_thread_list_lock_pre_unlock(void){}
+void get_thread_list_lock_pthread_exit(void){}
+void get_thread_list_lock_tid_overlimit(void){}
+void get_register_count(void){}
+void getDlcloseLockStatus(void){}
+void getDlcloseLockLastExitTid(void){}
